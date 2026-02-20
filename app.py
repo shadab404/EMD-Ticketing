d24fd74c90e3f8ff358249f7b21caae103325474
shadab404 <amuejohn404@gmail.com>
2026-02-21 02:12:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 
 # --- CONFIGURATION ---
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-dev-key-123')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", "sqlite:///emdad.db"
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or "sqlite:///emdad.db"
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
