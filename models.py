20fd321ffdbd61f96a8ba347049ef4c63cd1646c
shadab404 <amuejohn404@gmail.com>
2026-02-23 12:41:09 +0300

    Update models.py

--- models.py
+++ models.py
@@ -8,12 +8,17 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default="agent")  # admin / agent
+
+    tickets = db.relationship('Ticket', backref='agent', lazy=True)
+
 
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    priority = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.String(50), default="Open")
+    priority = db.Column(db.String(20), nullable=False)
+    status = db.Column(db.String(20), default="Open")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    assigned_to = db.Column(db.String(100))
+    due_date = db.Column(db.DateTime)
+    agent_id = db.Column(db.Integer, db.ForeignKey('user.id'))
